50ac397781815ec42197ab074d56ca6fab394a5f
Jonathan Garuti <jonathan_garuti@hotmail.com>
2024-03-15 12:19:07 -0300

    :construction: CRUD Iniciado

---
+++ src/main/java/com/generation/greengen/repository/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.generation.greengen.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.greengen.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	 public List <Categoria> findAllNomeContainingIgnoreCase(@Param("nome")String nome); 
+}
